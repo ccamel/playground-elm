1c90ac3801e9fda32de8431b1284a37fa595c7e5
Chris Camel <camel.christophe@gmail.com>
2017-07-31 12:18:24 +0200

    add the "Fork me on GitHub" ribbon

--- src/elm/App/View.elm
+++ src/elm/App/View.elm
@@ -1,7 +1,7 @@
 module App.View exposing (..)
 
-import Html exposing (Html, div, text, button, h1, p)
-import Html.Attributes exposing (class)
+import Html exposing (Html, a, button, div, h1, img, p, text)
+import Html.Attributes exposing (alt, attribute, class, href, src)
 import Html.Events exposing (onClick)
 import App.Messages exposing (Msg(..))
 import App.Models exposing (Model)
@@ -30,7 +30,10 @@ page model =
 mainPage : Html Msg
 mainPage =
     div [ class "jumbotron" ]
-        [ div [ class "container" ]
+        [
+          forkMeOnGitHub,
+
+          div [ class "container" ]
             [ h1 [] [ text "Welcome to Elm Main page" ]
             , p [] [ text "A delightful language for reliable webapps." ]
             , button [ onClick (GoToPage About), class "btn btn-primary btn-lg" ] [ text "Go To About Page" ]
@@ -41,7 +44,10 @@ mainPage =
 aboutPage : Html Msg
 aboutPage =
     div [ class "jumbotron" ]
-        [ div [ class "container" ]
+        [
+          forkMeOnGitHub,
+
+          div [ class "container" ]
             [ h1 [] [ text "This is <about> page" ]
             , button [ onClick (GoToPage Home), class "btn btn-primary btn-lg" ] [ text "Go To Home Page" ]
             ]
@@ -52,3 +58,15 @@ notFoundView : Html msg
 notFoundView =
     div []
         [ text "Not Found" ]
+
+forkMeOnGitHub : Html msg
+forkMeOnGitHub =
+      a [ href "https://github.com/ccamel/playground-elm" ]
+        [ img [ alt "Fork me on GitHub",
+                attribute "data:data-canonical-src" "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+              , src "https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+              , attribute "style" "position: absolute; top: 0; right: 0; border: 0;"
+              ]
+           []
+        , text ""
+        ]
