2dcc5d8b68734b7537c8681cc7ee1cb333881be4
Chris Camel <camel.christophe@gmail.com>
2017-08-23 21:36:23 +0200

    :arrow_up: upgrade npm dependencies to latest

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
             {
                 test: /\.elm$/,
                 exclude: [/elm-stuff/, /node_modules/],
-                loader: 'elm-webpack?verbose=true&warn=true'
+                loader: 'elm-webpack-loader?verbose=true&warn=true'
             },
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
