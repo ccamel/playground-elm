bd498fbcfe4f9de5b78f7223ccd3f6acd01178ab
Chris Camel <camel.christophe@gmail.com>
2020-12-24 22:12:50 +0100

    :heavy_plus_sign: add elm-canvas library

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require('./fonts/font.css');
 require('tether/dist/css/tether.css');
 require('bootstrap/dist/css/bootstrap.css');
 require('bootstrap/dist/js/bootstrap.bundle.js');
+require('elm-canvas/elm-canvas.js');
 require('font-awesome/css/font-awesome.css');
 require('animate.css/animate.css')
 require('./playground.css');
